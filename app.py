82e278c62fd57ee8736941682158174ab6d99758
poojagaikwad307 <poojagaikwad976572@gmail.com>
2025-04-07 05:21:02 +0000

    app ready to deployment

--- app.py
+++ app.py
@@ -6,5 +6,31 @@ from utils import api_extracter
 # Cerate an instance/object of the class
 client = api_extracter()
 
+# Create webpage title
+st.set_page_config(page_title="IMDB Top Movies Project",layout="wide")
+
 # Cerate website title
-st.title("IMDB top Movies Project")
+st.title("IMDB top Movies App")
+
+st.subheader("By Sindhura Nadendla")
+
+# create a button which prompts user about top movies data
+button = st.button("Click here to view most popular movie titles")
+
+# action to be performed when above button is clicked
+if button:
+    data = client.get_data()
+    #st.text(data)
+    #create a dataframe to display the data collected in tabular format
+    df = pd.DataFrame(data,columns=["Movie Names","Release Year","Rank"])
+    st.dataframe(df)
+
+    csv_data = df.to_csv(index=False).encode("utf-8")
+
+    # TO include a download button
+    st.download_button(
+        label = "Download above content as csv file",
+        data = csv_data,
+        file_name = "IMDB Popular Movie data.csv",
+        mime = "text/csv"
+    )
